6219c5a4c7bd7557993a60c9c130e5d4a0902a0d
senfirst <ahnethiernodjiby@gmail.com>
2026-09-01 15:28:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,19 @@ const supabase = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+async function testerConnexion() {
+    const { data, error } = await supabase
+        .from("profiles")
+        .select("*")
+        .limit(5);
+
+    if (error) {
+        console.error("❌ Erreur Supabase :", error);
+        return;
+    }
+
+    console.log("✅ Connexion réussie :", data);
+}
+
+testerConnexion();
